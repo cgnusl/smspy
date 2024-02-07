e652bbd1b97add61eb94ca53bade0bbf68e9c536
cgnusl <121693611+cgnusl@users.noreply.github.com>
2024-02-07 23:43:14 +0300

    Update sms.py

--- sms.py
+++ sms.py
@@ -13,7 +13,7 @@ from time import sleep
 new_title = "Hoşgeldiniz! / SMS Bomber v1.1 by CGNUSL"
 
 # Konsol penceresine başlığı uygula
-set_console_title(new_title)
+
 
 password_url = "https://careful-charming-trigonometry.glitch.me/getpass"
 
