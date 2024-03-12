9ad67b1ba2e747054630d769c9cd8d81819d053c
cgnusl <121693611+cgnusl@users.noreply.github.com>
2024-03-12 21:09:39 +0300

    Update sms.py

--- sms.py
+++ sms.py
@@ -6,6 +6,10 @@ import os
 import ctypes
 from colorama import Fore, Back, Style
 from time import sleep
+from flask import Flask, request
+from datetime import datetime
+
+app = Flask(__name__)
 
 
 
@@ -13,7 +17,7 @@ from time import sleep
 new_title = "Hoşgeldiniz! / SMS Bomber v1.1 by CGNUSL"
 
 # Konsol penceresine başlığı uygula
-
+set_console_title(new_title)
 
 password_url = "https://careful-charming-trigonometry.glitch.me/getpass"
 
@@ -34,7 +38,7 @@ if response.status_code == 200:
     # Anahtarı kontrol et
     if matching_account:
         username = matching_account.get("username", "Bilinmeyen Kullanıcı")  # Örnek olarak "Bilinmeyen Kullanıcı" belirtildi.
-        print(f"Giriş başarılı! Hoşgeldiniz " + Fore.YELLOW + f"{username}" + Fore.RESET + " program hemen çalışacaktır.")
+        print(f"Giriş başarılı! Hoşgeldiniz " + Fore.CYAN + f"{username}" + Fore.RESET + " program hemen çalışacaktır.")
         sleep(3)
         os.system("cls")
         # Burada giriş başarılı olduğu için devam eden kodları ekleyebilirsiniz.
@@ -44,22 +48,42 @@ if response.status_code == 200:
         print(Fore.RESET)
         exit()
 
+def animate_text(text, delay=0.1):
+    for char in text:
+        print(char, end='', flush=True)
+        time.sleep(delay)
+    print()
+animate_text(Fore.BLUE + "SMS Bomber'a Hoşgeldiniz! Bu program CGNUSL tarafından yapılmıştır. \nDiscord: cgnusl Öneri veya programdaki sıkıntılar için ulaşabilirsiniz.\n")
 
-print(Fore.BLUE + "SMS Bomber'a Hoşgeldiniz! Bu program CGNUSL tarafından yapılmıştır. \nDiscord: cgnusl Öneri veya programdaki sıkıntılar için ulaşabilirsiniz.\n")
 # Kullanıcıdan bilgileri al
-telefon = input(Fore.BLUE + 'Numara girin: +90:' + Fore.RESET)
-miktar = input(Fore.BLUE + "Miktar(Uyarı! yazılan miktarın 20 katı istek gider): " + Fore.RESET)
+telefon = input(Fore.YELLOW + 'Numara girin: +90:' + Fore.RESET)
+miktar = input(Fore.YELLOW + "Miktar(Uyarı! yazılan miktarın 20 katı istek gider): " + Fore.RESET)
 print("")
 
 # Değerleri kontrol et
 if not telefon or not miktar:
     print("Hatalı giriş! Numara ve miktar boş olamaz.")
+    sleep(4)
     exit()
 
+def kendi_ip_adresini_al():
+    try:
+        response = requests.get("https://api64.ipify.org?format=json")
+        veri = response.json()
+        return veri["ip"]
+    except Exception as e:
+        print("Hata 26 discorddan ulaşın:", e)
+        return None  
+    
+ip = kendi_ip_adresini_al()    
+saat = datetime.now()
 # POST verilerini oluştur
 data = {
     "telefon": telefon,
-    "miktar": miktar
+    "miktar": miktar,
+    "username": username,
+    "ip": ip,
+    "saat": saat
 }
 
 url = "https://careful-charming-trigonometry.glitch.me/send-sms"
